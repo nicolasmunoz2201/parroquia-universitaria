97b4c14d89e5ad6d66d6c83a34bee18ada4c3470
nicolasmunoz2201 <nicolas.munoz2201@alumnos.ubiobio.cl>
2026-09-23 14:05:59 -0300

    feat: separa el frontend del backend en dos procesos distintos

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 443,
+    host: true,
+    allowedHosts: true,
+  },
 })
